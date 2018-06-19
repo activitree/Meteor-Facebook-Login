5c6d165144c69841438ef2ff158882df0d5b1b38
Paul Ailincai <paulailincai@icloud.com>
2018-06-20 01:26:28 +0400

    Update the global object

--- dist/init.js
+++ dist/init.js
@@ -1,4 +1,11 @@
 /* globals BasMTR */
 'use strict'
 
+if (typeof global !== 'undefined' && typeof global.BasMTR === 'undefined') {
+  global.BasMTR = {}
+}
+if (typeof window !== 'undefined' && typeof window.BasMTR === 'undefined') {
+  window.BasMTR = {}
+}
+
 BasMTR.FB_API = {}
